40d5663862351ceca8f442f9166b54df374fe3f3
Avinash Malhotra <avimalhotra505@gmail.com>
2020-07-12 13:05:18 +0530

    app.js
    test file in node REPL

---
+++ app.js
@@ -0,0 +1,9 @@
+const pi=Math.PI;
+let r=15;
+
+//let area=pi*r*r;
+//let circ=pi*2*r;
+//console.log(area.toFixed(2), circ.toFixed(2));
+//console.log(`Area is ${area.toFixed(2)} and circ is ${circ.toFixed(2)}`)
+
+console.log(` r is ${r}, Area is ${ (pi*r*r).toFixed(2) } and circ is ${ (pi * 2 * r).toFixed(2)}`)
