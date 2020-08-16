b2981edff1f9947f79370df8e932bbecb2c8f0f2
Avinash Malhotra <avimalhotra505@gmail.com>
2020-08-16 13:15:38 +0530

    Add files via upload

--- src/server.js
+++ src/server.js
@@ -1,31 +1,36 @@
 const express=require('express');
-
 let app=express();
-
-// express().use((req,res)=>{
-//     res.end('hello world')
-// }).listen(8080);
 
 //app.use(express.static('src/public'));
 
 app.use((req,res,next)=>{
-    console.log("app running");
+    console.log('Session starts at : %d', Date.now());
     next()
 });
 
 app.get("/",(req,res)=>{
-    //console.log(req.url);
     res.status(200).send("hello express");
 });
-
-app.post("/postdata",(req,res)=>{
-    res.status(200).send("hello express post");
-});
-
 app.get("/admin",(req,res)=>{
-    //console.log(req.url);
     res.status(200).send("hello admin");
 });
+
+
+app.get("/tv/:brand/:model",(req,res)=>{
+    res.send(req.params);
+})
+
+
+// for form
+app.get("/login",(req,res)=>{
+   // res.send(`Thanks ${req.query.username}.`);
+    res.json({"data":req.query})
+});
+
+app.post("/login",(req,res)=>{
+    res.send(`Thanks for your query`);
+})
+
 
 
 
@@ -33,6 +38,6 @@ app.get("/admin",(req,res)=>{
 app.get('/**',(req,res)=>{
     res.status(404).send("PAGE not found");
 });
-app.listen(8080,()=>{
-    console.log(`server running at http://127.0.0.1:8080`)
+app.listen(3000,()=>{
+    console.log(`server running at http://127.0.0.1:3000`)
 })
